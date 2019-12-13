b8f5983a1d8b5e9e24e0f5be852085ab4de19ee6
atleastzero <megan.obryan52@gmail.com>
2019-12-12 16:18:36 -0800

    Starting over.

--- xyearplan/flowcharts/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FlowchartsConfig(AppConfig):
-    name = 'flowcharts'
--- xyearplan/flowcharts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Course(models.Model):
-    code_subject = models.CharField(max_length=10)
-    code_number = models.IntegerField(default=100)
-    name = models.CharField(max_length=100)
-    credit_hours = models.IntegerField()
